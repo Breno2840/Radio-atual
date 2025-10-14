8acbff4199f9988dae1ff262febf2ee37d8d245d
Breno2840 <128945701+Breno2840@users.noreply.github.com>
2025-10-14 01:23:20 -0300

    Atualizar o settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,13 +1,5 @@
 pluginManagement {
-    val flutterSdkPath = run {
-        val properties = java.util.Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
-        val flutterSdkPath = properties.getProperty("flutter.sdk")
-        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-        flutterSdkPath
-    }
-
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+    includeBuild("../flutter_modules/flutter_plugin_loader")
 
     repositories {
         google()
@@ -18,8 +10,10 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false // <-- CORRIGIDO
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
 include(":app")
+// A LINHA MAIS IMPORTANTE QUE FALTAVA:
+includeBuild("../flutter_modules/flutter_engine")
