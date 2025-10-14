53cd6db4da41af90df79aec0c729fc88a7282120
Breno2840 <128945701+Breno2840@users.noreply.github.com>
2025-10-14 10:08:50 -0300

    Atualizar o build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,21 @@
-plugins {
-    // Este arquivo pode ficar assim, quase vazio.
-    // As versões dos plugins agora são gerenciadas no settings.gradle.kts
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
